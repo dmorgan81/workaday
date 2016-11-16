a6a45b39754258244d7865da05906eefff9eeb27
David Morgan <dmorgan81@gmail.com>
2016-11-16 10:22:53 -0500

    Connection & Hourly vibes

--- src/c/workaday.c
+++ src/c/workaday.c
@@ -1,5 +1,7 @@
 #include <pebble.h>
 #include <pebble-events/pebble-events.h>
+#include <pebble-connection-vibes/connection-vibes.h>
+#include <pebble-hourly-vibes/hourly-vibes.h>
 #include "logging.h"
 #include "enamel.h"
 #include "colors.h"
@@ -11,6 +13,8 @@ static EventHandle s_settings_event_handle;
 static void settings_handler(void *context) {
     log_func();
     window_set_background_color(s_window, colors_get_background_color());
+    connection_vibes_set_state(atoi(enamel_get_CONNECTION_VIBE()));
+    hourly_vibes_set_enabled(enamel_get_HOURLY_VIBE());
 }
 
 static void window_load(Window *window) {
@@ -27,6 +31,18 @@ static void window_unload(Window *window) {
 static void init(void) {
     log_func();
     enamel_init();
+    connection_vibes_init();
+    hourly_vibes_init();
+    uint32_t const pattern[] = { 100 };
+    hourly_vibes_set_pattern((VibePattern) {
+        .durations = pattern,
+        .num_segments = 1
+    });
+
+#ifdef PBL_HEALTH
+    connection_vibes_enable_health(true);
+    hourly_vibes_enable_health(true);
+#endif
 
     events_app_message_open();
 
@@ -42,6 +58,8 @@ static void deinit(void) {
     log_func();
     window_destroy(s_window);
 
+    hourly_vibes_deinit();
+    connection_vibes_deinit();
     enamel_deinit();
 }
 
