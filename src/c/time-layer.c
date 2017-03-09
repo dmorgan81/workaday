6197f98e43747d2f2be35b7f4b8be5979ac781e0
David Morgan <dmorgan81@gmail.com>
2017-03-09 10:56:09 -0500

    Show seconds setting

--- src/c/time-layer.c
+++ src/c/time-layer.c
@@ -6,7 +6,7 @@
 #include "time-layer.h"
 
 typedef struct __attribute__((packed)) {
-    char buf[6];
+    char buf[9];
     FFont *font;
     EventHandle settings_event_handle;
     EventHandle tick_timer_event_handle;
@@ -17,20 +17,21 @@ static void update_proc(Layer *this, GContext *ctx) {
     Data *data = layer_get_data(this);
     GRect bounds = layer_get_unobstructed_bounds(this);
     FPoint center = FPointI(bounds.size.w / 2, bounds.size.h / 2 - PBL_IF_DISPLAY_LARGE_ELSE(7, 5));
-    fixed_t width = INT_TO_FIXED(bounds.size.w);
-
-    int16_t font_size = PBL_IF_DISPLAY_LARGE_ELSE(80, 60);
 
     FContext fctx;
     fctx_init_context(&fctx, ctx);
 
+    int16_t font_size = PBL_IF_DISPLAY_LARGE_ELSE(80, 60);
     fixed_t str_width;
+    fixed_t width = INT_TO_FIXED(bounds.size.w);
+
     do {
         fctx_set_text_em_height(&fctx, data->font, font_size--);
         str_width = fctx_string_width(&fctx, data->buf, data->font);
     } while (str_width > width);
 
     fctx_set_text_em_height(&fctx, data->font, ++font_size);
+
     fctx_set_color_bias(&fctx, 0);
     fctx_set_fill_color(&fctx, colors_get_foreground_color());
     fctx_set_offset(&fctx, center);
@@ -46,11 +47,15 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed, void *th
     log_func();
     Data *data = layer_get_data(this);
 #ifndef DEMO
+    char s[6];
     if (enamel_get_LEADING_ZERO()) {
-        strftime(data->buf, sizeof(data->buf), clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time);
+        snprintf(s, sizeof(s), clock_is_24h_style() ? "%%H:%%M" : "%%I:%%M");
     } else {
-        strftime(data->buf, sizeof(data->buf), clock_is_24h_style() ? "%k:%M" : "%l:%M", tick_time);
+        snprintf(s, sizeof(s), clock_is_24h_style() ? "%%k:%%M" : "%%l:%%M");
     }
+    char t[10];
+    snprintf(t, sizeof(t), "%s%s", s, enamel_get_SHOW_SECONDS() ? ":%S" : "");
+    strftime(data->buf, sizeof(data->buf), t, tick_time);
 #else
     snprintf(data->buf, sizeof(data->buf), "12:34");
 #endif
@@ -59,8 +64,14 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed, void *th
 
 static void settings_handler(void *this) {
     log_func();
+    Data *data = layer_get_data(this);
+
+    if (data->tick_timer_event_handle) events_tick_timer_service_unsubscribe(data->tick_timer_event_handle);
+
     time_t now = time(NULL);
-    tick_handler(localtime(&now), MINUTE_UNIT, this);
+    TimeUnits units = enamel_get_SHOW_SECONDS() ? SECOND_UNIT : MINUTE_UNIT;
+    tick_handler(localtime(&now), units, this);
+    data->tick_timer_event_handle = events_tick_timer_service_subscribe_context(units, tick_handler, this);
 }
 
 TimeLayer *time_layer_create(GRect frame) {
@@ -73,7 +84,6 @@ TimeLayer *time_layer_create(GRect frame) {
 
     settings_handler(this);
     data->settings_event_handle = enamel_settings_received_subscribe(settings_handler, this);
-    data->tick_timer_event_handle = events_tick_timer_service_subscribe_context(MINUTE_UNIT, tick_handler, this);
 
     return this;
 }
@@ -81,7 +91,7 @@ TimeLayer *time_layer_create(GRect frame) {
 void time_layer_destroy(TimeLayer *this) {
     log_func();
     Data *data = layer_get_data(this);
-    events_tick_timer_service_unsubscribe(data->tick_timer_event_handle);
+    if (data->tick_timer_event_handle) events_tick_timer_service_unsubscribe(data->tick_timer_event_handle);
     enamel_settings_received_unsubscribe(data->settings_event_handle);
     ffont_destroy(data->font);
     layer_destroy(this);
